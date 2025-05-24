1c8016cbfbd946b9563874fcbef465dcdb10e9a5
Guilherme Oliveira <dev.guilherme7@gmail.com>
2025-05-24 10:26:46 -0300

    feat: Configure stylelint for CSS/SCSS linting
    This commit introduces stylelint to enforce consistent styling and best practices in CSS and SCSS files.

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,5 @@
 export default {
   '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
-  '*.{json,md,css,html,yml,yaml,scss}': ['prettier --write'],
+  '*.{css,scss}': ['stylelint --fix'],
+  '*.{json,md,html,yml,yaml}': ['prettier --write'],
 };
